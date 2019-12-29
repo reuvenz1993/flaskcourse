eadb717a60cf1ee4b22504fcb594dfdb28b7aed0
reuvenz1993 <rovenroven1@gmail.com>
2019-12-29 18:43:06 +0200

    asd

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from trivia import app,db
 from flask import render_template, redirect, request, url_for, flash,abort
-from flask_login import login_user,login_required,logout_user, current_user
-from trivia.models import User
-from trivia.forms import LoginForm, SignupForm
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+def index_trivia():
+    return render_template('index_trivia.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- trivia/__init__.py
+++ trivia/__init__.py
@@ -5,8 +5,6 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 
-# Create a login manager object
-login_manager = LoginManager()
 
 app = Flask(__name__)
 
@@ -18,9 +16,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app,db)
-
-# We can now pass in our app to the login manager
-login_manager.init_app(app)
-
-# Tell users what view to go to when they need to login.
-login_manager.login_view = "index"
