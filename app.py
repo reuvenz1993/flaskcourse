294d4a73fe6be4f7a599bbad81e488d70ad94a9d
reuvenz1993 <rovenroven1@gmail.com>
2020-01-02 21:15:25 +0200

    views

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-#import requests
 from trivia import app,db
-#from flask import render_template, redirect, request, url_for, flash,abort , session  , escape , make_response , jsonify
-#import json
-#from trivia.models import Score
-#from sqlalchemy import desc
-
 import trivia.views
 
 if __name__ == '__main__':
